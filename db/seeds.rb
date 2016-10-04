750922501aa113d5d1ea1be5c04a872e45c0c6de
lukin41280 <jpelletier19@me.com>
2016-10-04 15:33:30 -0400

    populate seed file for club names and all distances from 50 yards to 250

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,29 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Club.create!({name: "2W"})
+Club.create!({name: "3W"})
+Club.create!({name: "4W"})
+Club.create!({name: "5W"})
+Club.create!({name: "6W"})
+Club.create!({name: "7W"})
+Club.create!({name: "2H"})
+Club.create!({name: "3H"})
+Club.create!({name: "4H"})
+Club.create!({name: "5H"})
+Club.create!({name: "6H"})
+Club.create!({name: "2I"})
+Club.create!({name: "3I"})
+Club.create!({name: "4I"})
+Club.create!({name: "5I"})
+Club.create!({name: "6I"})
+Club.create!({name: "7I"})
+Club.create!({name: "8I"})
+Club.create!({name: "9I"})
+Club.create!({name: "PW"})
+Club.create!({name: "GW"})
+Club.create!({name: "SW"})
+Club.create!({name: "AW"})
+
+distance = 50
+while distance < 251
+  Distance.create!({yards: distance})
+  distance += 1
+end
